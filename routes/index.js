6effced54bd4c3bc1aa577455489119b9b5f5ae4
Song <yousonghou@yahoo.com>
2018-02-11 03:06:48 +0000

    Improve back button for comment

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,14 @@ const router = express.Router();
 const passport = require("passport");
 const User = require("../models/user");
 
-// Root Route
 router.get("/", function(req, res){
     res.render("home");
 });
 
-// AUTH ROUTES
-// Show register form
 router.get("/register", function(req, res){
     res.render("register");
 });
 
-// Handle sign up logic
 router.post("/register", function(req, res){
     let newUser = new User({username: req.body.username});
     User.register(newUser, req.body.password, function(err, user){
@@ -29,12 +25,10 @@ router.post("/register", function(req, res){
     });
 });
 
-// Show login form
 router.get("/login", function(req, res){
     res.render("login");
 });
 
-// Handling login logic
 router.post("/login", passport.authenticate("local", 
     {
         successRedirect: "/",
@@ -42,7 +36,6 @@ router.post("/login", passport.authenticate("local",
     }), function(req, res){
 });
 
-// Logic route
 router.get("/logout", function(req, res){
     req.logout();
     req.flash("success", "Logged you out");
@@ -50,14 +43,18 @@ router.get("/logout", function(req, res){
     
 });
 
-// Edit Profile
 router.get("/profile/:id", function(req, res){
     User.findById(req.params.id, function(err, foundUser){
-        res.render("profile", {user: foundUser});
+        if(err){
+            req.flash("error", err);
+            res.redirect("back");
+        }
+        else{
+            res.render("profile", {user: foundUser});
+        }
     });
 });
 
-// Update Profile
 router.put("/profile/:id", function(req, res){
     User.findByIdAndUpdate(req.params.id, req.body.user, function(err, updatedUser){
         if(err){
